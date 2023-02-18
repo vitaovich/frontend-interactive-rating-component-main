a022a59a8a064a8b5ca3052dc1927eca5b76c43c
vitaovich <vitaliyalekhnovich@gmail.com>
2023-02-18 18:02:42 +0000

    update to use @next/font/google for Overpass

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
+import { Overpass } from '@next/font/google'
 import type { AppProps } from 'next/app'
 
+const overpass = Overpass({
+  subsets: ['latin'],
+  variable: '--font-overpass'
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${overpass.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
   theme: {
     extend: {},
     fontFamily: {
-      overpass: ['Overpass', 'sans-sarif'],
+      overpass: ['var(--font-overpass)'],
     },
   },
   plugins: [],
