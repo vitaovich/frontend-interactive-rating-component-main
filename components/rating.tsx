d0b849afa2364aa248790b0bb8f56c6de653ea54
vitaovich <vitaliyalekhnovich@gmail.com>
2023-02-19 06:01:26 +0000

    Add submitted section and move rate handling to Home page

--- components/rating.tsx
+++ components/rating.tsx
@@ -1,19 +1,18 @@
 import { useState } from "react";
 import RatingButton from "./ratingButton";
 
-const Rating = () => {
+const Rating: React.FC<{ rating: number, updateRateHandler: (rating: number) => void }> = (props) => {
     const [ratings, setRatings] = useState([1,2,3,4,5])
-    const [curRating, setRating] = useState<number>(0)
 
     const ratingHandler = (rating: number) => {
-        setRating(rating)
+        props.updateRateHandler(rating)
     }
     
     return (
         <div className="flex flex-row justify-center text-center text-lightGrey space-x-4 md:space-x-10">
             {
                 ratings.map((rating) => (
-                    <RatingButton key={rating} rating={rating} onRatingClick={ratingHandler} className={curRating == rating ? 'bg-mediumGrey text-white' : 'bg-darkBlue'}></RatingButton>
+                    <RatingButton key={rating} rating={rating} onRatingClick={ratingHandler} className={props.rating == rating ? 'bg-mediumGrey text-white' : 'bg-darkBlue'}></RatingButton>
                 ))
             }
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,38 +2,61 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 import Rating from '@/components/rating'
+import { useState } from 'react'
 
 export default function Home() {
+  const [rating,setRating] = useState<number>(0)
+  const [submitted, setSubmitted] = useState<boolean>(false)
+
+  const rateHandler = (rating:number) => {
+    setRating(rating)
+  }
+
+  const onSubmitHandler = () => {
+    setSubmitted(!submitted)
+  }
+
   return (
     <>
       <div className="flex items-center justify-center min-h-screen bg-veryDarkBlue text-white">
-        <div className="flex flex-col space-y-4 bg-gradient-to-b from-darkBlueTop to-darkBlueBottom p-4 m-4 rounded-xl md:w-1/3">
-          <div>
-            <button className="flex items-center justify-center w-12 h-12 rounded-full bg-darkBlue p-4">
-              <Image
-                alt='star'
-                src='/images/icon-star.svg'
-                width={16}
-                height={16}
-                className='text-orange'
-              />
-            </button>
-          </div>
-          <h1 className="text-2xl font-bold">How did we do?</h1>
-          <p className="text-lightGrey">
-            Please let us know how we did with your support request. All feedback is appreciated
-            to help us improve our offering!
-          </p>
-          <Rating></Rating>
-          <button className='bg-orange rounded-full py-3 tracking-widest hover:bg-white hover:text-orange'><span className="uppercase font-bold">Submit</span></button>
-          {/* <!-- Rating state end --> */}
-          {/* <!-- Thank you state start --> */}
-          {/* You selected <!-- Add rating here --> out of 5 */}
-          {/* Thank you!
-          We appreciate you taking the time to give a rating. If you ever need more support,
-          don’t hesitate to get in touch! */}
-          {/* <!-- Thank you state end --> */}
+        <div className={`flex flex-col space-y-4 bg-gradient-to-b from-darkBlueTop to-darkBlueBottom p-4 m-4 rounded-xl md:w-1/3 ${submitted ? 'hidden' : ''}`}>
+            <div>
+              <button className="flex items-center justify-center w-12 h-12 rounded-full bg-darkBlue p-4">
+                <Image
+                  alt='star'
+                  src='/images/icon-star.svg'
+                  width={16}
+                  height={16}
+                  className='text-orange'
+                />
+              </button>
+            </div>
+            <h1 className="text-2xl font-bold">How did we do?</h1>
+            <p className="text-lightGrey">
+              Please let us know how we did with your support request. All feedback is appreciated
+              to help us improve our offering!
+            </p>
+            <Rating rating={rating} updateRateHandler={rateHandler} ></Rating>
+            <button className='bg-orange rounded-full py-3 tracking-widest hover:bg-white hover:text-orange' onClick={onSubmitHandler}><span className="uppercase font-bold">Submit</span></button>
         </div>
+        {/* <!-- Rating state end --> */}
+        {/* <!-- Thank you state start --> */}
+        <div className={`flex flex-col items-center space-y-6 bg-gradient-to-b from-darkBlueTop to-darkBlueBottom p-8 m-4 rounded-xl md:w-1/3  ${submitted ? '' : 'hidden'}`}>
+          <Image
+            alt='illustration thank you'
+            src='/images/illustration-thank-you.svg'
+            width={162}
+            height={108}
+            className='text-orange'
+          />
+            <span className="text-center text-orange rounded-full bg-darkBlue px-5 py-2">You selected {rating} out of 5</span>
+            <h1 className="text-3xl text-white">Thank you!</h1>
+            <p className="text-lightGrey text-center">
+              We appreciate you taking the time to give a rating. If you ever need more support,
+              don’t hesitate to get in touch!
+            </p>
+        </div>
+        {/* <!-- Thank you state end --> */}
       </div>
 
     </>
