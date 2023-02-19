7dd88c81c3d921506db05e6b76f6e588d69c1ec1
vitaovich <vitaliyalekhnovich@gmail.com>
2023-02-19 05:36:29 +0000

    add rating select logic using useRef and useState

--- components/rating.tsx
+++ components/rating.tsx
@@ -3,11 +3,17 @@ import RatingButton from "./ratingButton";
 
 const Rating = () => {
     const [ratings, setRatings] = useState([1,2,3,4,5])
+    const [curRating, setRating] = useState<number>(0)
+
+    const ratingHandler = (rating: number) => {
+        setRating(rating)
+    }
+    
     return (
         <div className="flex flex-row justify-center text-center text-lightGrey space-x-4 md:space-x-10">
             {
                 ratings.map((rating) => (
-                    <RatingButton key={rating} rating={rating}></RatingButton>
+                    <RatingButton key={rating} rating={rating} onRatingClick={ratingHandler} className={curRating == rating ? 'bg-mediumGrey text-white' : 'bg-darkBlue'}></RatingButton>
                 ))
             }
         </div>
--- components/ratingButton.tsx
+++ components/ratingButton.tsx
@@ -1,6 +1,15 @@
-const RatingButton: React.FC<{ rating: number }> = (props) => {
+import { useRef } from "react"
+
+const RatingButton: React.FC<{ rating: number, className: string, onRatingClick: (rating: number) => void }> = (props) => {
+    const ratingRef = useRef<HTMLButtonElement>(null)
+
+    const handleRatingClick = () => {
+        if (ratingRef.current?.innerText !== undefined) {
+            props.onRatingClick(parseInt(ratingRef.current?.innerText))
+        }
+    }
     return (
-        <button className="flex items-center justify-center w-12 h-12 rounded-full bg-darkBlue p-6 hover:bg-orange hover:text-white"><span>{props.rating}</span></button>
+        <button className={`flex items-center justify-center w-12 h-12 rounded-full p-6 hover:bg-orange hover:text-white ${props.className}`} onClick={handleRatingClick} ref={ratingRef}>{props.rating}</button>
     )
 }
 
